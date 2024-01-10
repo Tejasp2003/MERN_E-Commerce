1dcbb10ed4963f1832b8e2a8db0c82925178b048
Tejasp2003 <tejasp200320@gmail.com>
2024-01-10 22:14:31 +0530

    Cookie error fix

--- server/utils/createToken.js
+++ server/utils/createToken.js
@@ -6,14 +6,11 @@ const generateToken = (res, userId) => {
   });
 
   res.cookie("jwt", token, {
-    httpOnly:"true", // this is to prevent cross site scripting attacks by preventing access to the cookie from javascript code
-    secure: process.env.NODE_ENV !== "development", // this is to ensure that the cookie is only sent over https in production
-    maxAge: 30 * 24 * 60 * 60 * 1000, // this is to set the cookie to expire in 30 days
-    sameSite: "None", // this is to ensure that the cookie is sent on cross origin requests
-
-
-    
-  })
+    httpOnly: true,
+    secure: true, // Ensure this is true if SameSite=None
+    sameSite: 'None', // This allows the cookie to be sent in cross-domain contexts
+    maxAge: 30 * 24 * 60 * 60 * 1000
+  });
 
   return token;
 };
