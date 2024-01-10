f14fce0a8e79bb6ff4100975e096f9dfdb6f4637
Tosha <pateltk245.ce@gmail.com>
2024-01-10 17:28:12 +0530

    backend rendered

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy:{
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000"
-    }
-  }
-})
+    proxy: {
+      "/api/": "https://e-kart-shop.onrender.com",
+    },
+  },
+});
