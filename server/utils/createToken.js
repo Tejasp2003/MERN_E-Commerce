75bd3b3ecf2e5e8032a1a64fe54812efacab784b
Tejasp2003 <tejasp200320@gmail.com>
2024-01-10 22:08:59 +0530

    Cookie error fix

--- server/utils/createToken.js
+++ server/utils/createToken.js
@@ -9,7 +9,10 @@ const generateToken = (res, userId) => {
     httpOnly:"true", // this is to prevent cross site scripting attacks by preventing access to the cookie from javascript code
     secure: process.env.NODE_ENV !== "development", // this is to ensure that the cookie is only sent over https in production
     maxAge: 30 * 24 * 60 * 60 * 1000, // this is to set the cookie to expire in 30 days
-    sameSite: "none" // this is to ensure that the cookie is sent only in a first party context and not in a third party context i.e it is not sent to another website when the user clicks on a link in the website that contains the cookie
+    sameSite: "None", // this is to ensure that the cookie is sent on cross origin requests
+
+
+    
   })
 
   return token;
