7968fa5bf62f4391e69a4ee574ba8772f1ade2d1
Tejasp2003 <tejasp200320@gmail.com>
2024-01-10 18:31:35 +0530

    Cors error

--- server/index.js
+++ server/index.js
@@ -1,42 +1,37 @@
-import express from 'express';
-import path from 'path';
-import dotnev from 'dotenv';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import connectDB from './config/db.js';
-import userRoutes from './routes/userRoutes.js';
-import categoryRoutes from './routes/categoryRoutes.js';
-import productRoutes from './routes/productRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import cors from 'cors';
+import express from "express";
+import path from "path";
+import dotnev from "dotenv";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import cors from "cors";
+const app = express();
 
+app.use(cors());
 
 dotnev.config();
 
-const port = process.env.PORT || 5000;  
+const port = process.env.PORT || 5000;
 connectDB();
-const app = express();
-const corsOptions = {
-  origin: "https://mern-ekart.vercel.app/",
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json()); // for parsing application/json
-app.use(express.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
-
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get("/api/config/paypal", (req, res) => {
-    res.send({clientId: process.env.PAYPAL_CLIENT_ID});
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
-app.use('/api/users', userRoutes);
-app.use('/api/category', categoryRoutes);
-app.use('/api/products', productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/orders", orderRoutes);
 
@@ -44,6 +39,5 @@ app.use("/api/orders", orderRoutes);
 // app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-}
-);
+  console.log(`Server is running on port ${port}`);
+});
